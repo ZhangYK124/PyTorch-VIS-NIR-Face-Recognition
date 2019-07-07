5ac578011f6ba4f6e89e11df34ea014b952bec65
HyoKong <hyokong0102@gmail.com>
2019-07-07 16:50:11 +0800

    dataloader

--- DataLoader.py
+++ DataLoader.py
@@ -16,21 +16,60 @@ from align.align_trans import get_reference_facial_points, warp_and_crop_face
 
 import pdb
 
-class DataLoader(data.Dataset):
+class Dataset(data.Dataset):
     def __init__(self):
         
         self.vis_root = '/media/hyo/文档/VIS-NIR/CASIA_VIS_NIR/NIR_Aligned/'
         self.nir_root = '/media/hyo/文档/VIS-NIR/CASIA_VIS_NIR/VIS_Aligned/'
-        self.vis_img_list = glob.glob(self.vis_root)
-        self.nir_img_list = glob.glob(self.nir_root)
+        self.vis_img_list = os.listdir(self.vis_root)
+        self.nir_img_list = os.listdir(self.nir_root)
+
+        self.tf = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            ]
+        )
         
     def __len__(self):
         return min(len(glob.glob(self.vis_root)),len(glob.glob(self.nir_root)))
     
     def __getitem__(self, index):
-        nir_img = Image.open(self.nir_img_list[index])
+        vis_img = Image.open(os.path.join(self.vis_root,self.vis_img_list[index],self.vis_img_list[index]+'.jpg'))
+        
+        nir_name = choice(self.nir_img_list)
+        nir_img = Image.open(os.path.join(self.nir_root,nir_name,nir_name+'.jpg'))
         nir_img = nir_img.convert("RGB")
-        vis_img = Image.open(choice(self.vis_img_list))
-        _ , nir_landmark = detect_faces(nir_img)
-        _ , vis_landmark = detect_faces(vis_img)
+        
+        vis_left_eye_img = Image.open(os.path.join(self.vis_root,self.vis_img_list[index],'left_eye.jpg'))
+        vis_right_eye_img = Image.open(os.path.join(self.vis_root,self.vis_img_list[index],'right_eye.jpg'))
+        
+        nir_left_eye_img = Image.open(os.path.join(self.nir_root,nir_name,'left_eye.jpg'))
+        nir_right_eye_img = Image.open(os.path.join(self.nir_root,nir_name,'right_eye.jpg'))
+        
+        seed = random.random()
+        if seed>0.5:
+            temp = vis_left_eye_img
+            vis_left_eye_img = vis_right_eye_img
+            vis_right_eye_img = temp
+            
+            temp = nir_left_eye_img
+            nir_left_eye_img = nir_right_eye_img
+            nir_right_eye_img = temp
+            
+            vis_left_eye_img = vis_right_eye_img.transpose(Image.FLIP_LEFT_RIGHT)
+            vis_right_eye_img = vis_right_eye_img.transpose(Image.FLIP_LEFT_RIGHT)
+            nir_left_eye_img = nir_left_eye_img.transpose(Image.FLIP_LEFT_RIGHT)
+            nir_right_eye_img = nir_right_eye_img.transpose(Image.FLIP_LEFT_RIGHT)
+      
+        batch = {}
+        batch['vis'] = vis_img
+        batch['nir'] = nir_img
+        batch['vis_left_eye'] = vis_left_eye_img
+        batch['vis_right_eye'] = vis_right_eye_img
+        batch['nir_left_eye'] = nir_left_eye_img
+        batch['nir_right_eye'] = nir_right_eye_img
+        for k in batch:
+            batch[k] = self.tf(batch[k])
+        return batch
         
