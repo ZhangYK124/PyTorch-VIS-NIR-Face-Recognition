bb760f2be48b3b85a0484e5eaa0f36a67ee903f7
= <=>
2019-07-08 13:14:47 +0800

    update config

--- config.py
+++ config.py
@@ -1,8 +1,30 @@
 # TRAIN configuration
 train = {}
+train['train'] = True
 train['learning_rate'] = 1e-4
-train['num_epochs'] = 200
+train['epochs'] = 200
 train['batch_size'] = 32
-train['resume_model'] = None
-train['resume_optimizer'] = None
+
+train['lr_D_N'] = 1e-4
+train['beta1_D_N'] = 0.99
+train['beta2_D_N'] = 0.999
+train['weight_decay_D_N'] = 1e-5
+
+train['lr_D_V'] = 1e-4
+train['lr_G_N2V'] = 1e-4
+train['lr_G_V2N'] = 1e-4
+
+train['resume_G_N2V'] = None
+train['resume_G_V2N'] = None
+train['resume_D_N'] = None
+train['resume_D_V'] = None
+
+train['random_seed'] = None
+
+train['cuda'] = True
+
+# Evaluation configuration
+eval = {}
+eval['if_eval'] = True
+
 
