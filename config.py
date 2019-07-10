740c6db31cc67ea1459700c4dd2e2d85b51b53a8
= <=>
2019-07-11 00:12:00 +0800

    Update Version 1

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 train = {}
 train['if_train'] = True
 train['learning_rate'] = 1e-4
-train['epochs'] = 200
+train['epochs'] = 250
 train['batch_size'] = 6
 train['num_workers'] = 4
 
@@ -10,31 +10,31 @@ train['lambda_cyc_loss'] = 10.0
 train['lambda_id_loss'] = 5.0
 
 train['lr_D_N'] = 1e-3
-train['lr_D_N_decay_epoch'] = 100
+train['lr_D_N_decay_epoch'] = 20
 train['beta1_D_N'] = 0.99
 train['beta2_D_N'] = 0.999
 train['weight_decay_D_N'] = 1e-5
 
 train['lr_D_V'] = 1e-3
-train['lr_D_V_decay_epoch'] = 100
+train['lr_D_V_decay_epoch'] = 20
 train['beta1_D_V'] = 0.99
 train['beta2_D_V'] = 0.999
 train['weight_decay_D_V'] = 1e-5
 
 train['lr_G'] = 1e-3
-train['lr_G_decay_epoch'] = 100
+train['lr_G_decay_epoch'] = 20
 train['beta1_G'] = 0.99
 train['beta2_G'] = 0.999
 train['weight_decay_G'] = 1e-5
 
 train['lr_G_N2V'] = 1e-3
-train['lr_G_N2V_decay_epoch'] = 100
+train['lr_G_N2V_decay_epoch'] = 20
 train['beta1_G_N2V'] = 0.99
 train['beta2_G_N2V'] = 0.999
 train['weight_decay_G_N2V'] = 1e-5
 
 train['lr_G_V2N'] = 1e-3
-train['lr_G_V2N_decay_epoch'] = 100
+train['lr_G_V2N_decay_epoch'] = 20
 train['beta1_G_V2N'] = 0.99
 train['beta2_G_V2N'] = 0.999
 train['weight_decay_G_V2N'] = 1e-5
