7e74e65586d5b8373bb9c89e074d91358897d5f9
HyoKong <hyokong0102@gmail.com>
2019-09-29 23:54:29 +0800

    update

--- config_progresive.py
+++ config_progresive.py
@@ -6,20 +6,20 @@ train['saparately'] = False
 train['epochs'] = 2000
 train['batch_size'] = 2
 train['num_workers'] = 1
-train['generator_steps'] = 3
+train['generator_steps'] = 1
 
 train['lambda_rec'] = 10.0
 train['lambda_gauss'] = 0.0001
 train['lambda_cls'] = 1.0
 
-train['lr_D'] = 1e-3
+train['lr_D'] = 1e-4
 train['lr_D_decay_epoch'] = 1000
 train['lr_D_decay_rate'] = 0.998
 train['beta1_D'] = 0.5
 train['beta2_D'] = 0.999
 train['weight_decay_D'] = 1e-5
 
-train['lr_G'] = 1e-3
+train['lr_G'] = 1e-4
 train['lr_G_decay_epoch'] = 1000
 train['lr_G_decay_rate'] = 0.998
 train['beta1_G'] = 0.5
@@ -34,13 +34,8 @@ train['resume_optim_D'] = None
 
 train['checkpoint'] = './checkpoint_star/'
 train['logs'] = './logs_star/'
-train['out'] = '/media/hyo/文档/VIS-NIR/RESULT//out_star/'
+train['out'] = '/media/hyo/文档/VIS-NIR/RESULT/out_star/'
 
-# train['resume_G_N2V'] = 'checkpoint/G_N2V_20190807.pth'
-# train['resume_G_V2N'] = 'checkpoint/G_V2N_20190807.pth'
-# train['resume_D_N'] = 'checkpoint/D_N_20190807.pth'
-# train['resume_D_V'] = 'checkpoint/D_V_20190807.pth'
-#
 # # train['resume_optim_G_V2N'] = 'checkpoint/optimizer_G_V2N_20190807.pth'
 # # train['resume_optim_G_N2V'] = 'checkpoint/optimizer_G_N2V_20190807.pth'
 # train['resume_optim_G'] = 'checkpoint/optimizer_G_20190807.pth'
